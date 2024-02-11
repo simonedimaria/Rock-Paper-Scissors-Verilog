3c9e88f4fb98ebcc38022556138871209e2649e8
simonedimaria <12881861+simonedimaria@users.noreply.github.com>
2024-02-11 12:44:46 +0100

    clocking the assertions will sync everything...idk why

--- verilog/testbench.sv
+++ verilog/testbench.sv
@@ -7,6 +7,7 @@ module MorraCinese_tb;
   logic        INIZIA;
   logic [1:0] MANCHE;
   logic [1:0] PARTITA;
+  // for debug purposes 
   logic [4:0] max_manches, manches_played;
   logic [4:0] current_state, next_state;
   logic       moves_are_valid;
@@ -64,24 +65,18 @@ module MorraCinese_tb;
   end
   
   // Define tasks for setting inputs
-  task round;
+  task play_round_and_check;
     input logic [0:0] INIZIA_t;
     input logic [1:0] PRIMO_t;
     input logic [1:0] SECONDO_t;
-    begin
-      #2;
-      INIZIA  <= INIZIA_t;
-      PRIMO   <= PRIMO_t;
-      SECONDO <= SECONDO_t;
-    end
-  endtask
-  
-  // Asserts if the expected output is not received
-  task assert_output;
     input logic [1:0] expected_MANCHE;
     input logic [1:0] expected_PARTITA;
     begin
-      assert (MANCHE === expected_MANCHE)  else $error("Expected MANCHE:  %b, Received MANCHE:  %b, with inputs: INIZIA:%b, PRIMO:%b, SECONDO:%b", expected_MANCHE, MANCHE, INIZIA, PRIMO, SECONDO);   // @TO-DO: change manche_winner to MANCHE
+      INIZIA  <= INIZIA_t;
+      PRIMO   <= PRIMO_t;
+      SECONDO <= SECONDO_t;
+      @(posedge clk);
+      assert (MANCHE  === expected_MANCHE)  else $error("Expected MANCHE:  %b, Received MANCHE:  %b, with inputs: INIZIA:%b, PRIMO:%b, SECONDO:%b", expected_MANCHE, MANCHE, INIZIA, PRIMO, SECONDO);   // @TO-DO: change manche_winner to MANCHE
       assert (PARTITA === expected_PARTITA) else $error("Expected PARTITA: %b, Received PARTITA: %b, with inputs: INIZIA:%b, PRIMO:%b, SECONDO:%b", expected_PARTITA, PARTITA, INIZIA, PRIMO, SECONDO); // may be broken :'(
     end
   endtask
@@ -96,43 +91,43 @@ module MorraCinese_tb;
              );
 
     // Start
-    round(RESTART, 2'b10, 2'b01);       // setting n. of rounds to play: PRIMO+SECONDO + 4 = 1001 + 0100 = 13 rounds to play
-    assert_output(INVALID, NOT_ENDED);
+    play_round_and_check(RESTART, 2'b10, 2'b01, INVALID, NOT_ENDED);       // setting n. of rounds to play: PRIMO+SECONDO + 4 = 1001 + 0100 = 13 rounds to play
+    //assert_output(INVALID, NOT_ENDED);
     
     // Round 1 (0-0)
-    round(PLAY, NO_MOVE, NO_MOVE);
-    assert_output(INVALID, NOT_ENDED);
+    play_round_and_check(PLAY, NO_MOVE, NO_MOVE, INVALID, NOT_ENDED);
+    //assert_output(INVALID, NOT_ENDED);
     $display("Round 1");
 
     // Round 2 (1-0)
-    round(PLAY, PAPER, ROCK);
-    assert_output(PLAYER1, NOT_ENDED);
+    play_round_and_check(PLAY, PAPER, ROCK, PLAYER1, NOT_ENDED);
+    //assert_output(PLAYER1, NOT_ENDED);
     $display("Round 2");
 
     
     // Round 3 (1-1)
-    round(PLAY, SCISSORS, ROCK);
-    assert_output(PLAYER2, NOT_ENDED);
+    play_round_and_check(PLAY, SCISSORS, ROCK, PLAYER2, NOT_ENDED);
+    //assert_output(PLAYER2, NOT_ENDED);
     $display("Round 3");
     
     // Round 4 (1-1)
-    round(PLAY, NO_MOVE, PAPER);
-    assert_output(INVALID, NOT_ENDED);
+    play_round_and_check(PLAY, NO_MOVE, PAPER, INVALID, NOT_ENDED);
+    //assert_output(INVALID, NOT_ENDED);
     $display("Round 4");
     
     // Round 5 (1-1)
-    round(PLAY, ROCK, ROCK);
-    assert_output(NONE, NOT_ENDED);
+    play_round_and_check(PLAY, ROCK, ROCK, NONE, NOT_ENDED);
+    //assert_output(NONE, NOT_ENDED);
     $display("Round 5");
 
     // Round 6 (1-1)
-    round(PLAY, ROCK, ROCK);
-    assert_output(NONE, NOT_ENDED);
+    play_round_and_check(PLAY, ROCK, ROCK, NONE, NOT_ENDED);
+    //assert_output(NONE, NOT_ENDED);
     $display("Round 6");
 
     // Round 7 (2-1)
-    round(PLAY, PAPER, ROCK);
-    assert_output(PLAYER1, NOT_ENDED);
+    play_round_and_check(PLAY, PAPER, ROCK, PLAYER1, NOT_ENDED);
+    //assert_output(PLAYER1, NOT_ENDED);
     $display("Round 7");
     
     // more rounds
@@ -140,24 +135,24 @@ module MorraCinese_tb;
 
 
     // Restart the game
-    round(RESTART, 2'b00, 2'b01);     // setting n. of rounds to play: PRIMO+SECONDO + 4 = 0001 + 0100 = 5 rounds to play
-    assert_output(INVALID, NOT_ENDED);
+    play_round_and_check(RESTART, 2'b00, 2'b01, INVALID, NOT_ENDED);     // setting n. of rounds to play: PRIMO+SECONDO + 4 = 0001 + 0100 = 5 rounds to play
+    //assert_output(INVALID, NOT_ENDED);
 
     // Round 1 (0-1)
-    round(PLAY, ROCK, PAPER);
-    assert_output(PLAYER2, NOT_ENDED);
+    play_round_and_check(PLAY, ROCK, PAPER, PLAYER2, NOT_ENDED);
+    //assert_output(PLAYER2, NOT_ENDED);
 
     // Round 2 (0-2)
-    round(PLAY, SCISSORS, ROCK);
-    assert_output(PLAYER2, NOT_ENDED);
+    play_round_and_check(PLAY, SCISSORS, ROCK, PLAYER2, NOT_ENDED);
+    //assert_output(PLAYER2, NOT_ENDED);
 
     // Round 3 (0-3)
-    round(PLAY, PAPER, SCISSORS);
-    assert_output(PLAYER2, NOT_ENDED);
+    play_round_and_check(PLAY, PAPER, SCISSORS, PLAYER2, NOT_ENDED);
+    //assert_output(PLAYER2, NOT_ENDED);
 
     // Round 4 (1-3)
-    round(PLAY, SCISSORS, PAPER);
-    assert_output(PLAYER1, P2_WINNER);
+    play_round_and_check(PLAY, SCISSORS, PAPER, PLAYER1, NOT_ENDED);
+    //assert_output(PLAYER1, P2_WINNER);
     
     // Round 5 [...]
 
